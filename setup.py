5b7225320779299e4ba6d434802e7675ac3def16
Ben LaRoque <benjamin.laroque@gmail.com>
2015-10-23 15:33:55 -0700

    missed some top-level files

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+from glob import glob
+
+import sys
+from setuptools.command.test import test as TestCommand
+
+verstr = "none"
+try:
+    import subprocess
+    verstr = subprocess.check_output(['git','describe','--long']).decode('utf-8').strip()
+except EnvironmentError:
+    pass
+except Exception as err:
+    print(err)
+    verstr = 'v0.0.0-???'
+
+
+extras_require={
+    'doc': ['sphinx', 'sphinx_rtd_theme', 'sphinxcontrib-programoutput'],
+    'database': ['cython', 'psycopg2', 'sqlalchemy'], #this may also require system packages
+    'slack': ['slackclient'],
+    'dpph': ['numpy'],
+    'other': ['colorlog', 'ipython', 'ipdb'],
+}
+everything = set()
+for deps in extras_require.values():
+    everything.update(deps)
+extras_require['all'] = everything
+
+setup(
+    name='dragonfly',
+    version=verstr,
+    packages=['dragonfly','dragonfly/implementations'],
+    install_requires=['pika>=0.9.8', 'PyYAML', 'msgpack-python'],
+    extras_require=extras_require,
+    url='http://www.github.com/project8/dripline',
+    tests_require=['pytest'],
+)
