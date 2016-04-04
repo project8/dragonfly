f6c827fedfa081abd08b42f3e0564a3ccec15012
AliAshtariEsfahani <ashtari@uw.edu>
2016-04-04 14:54:08 -0700

    The automatic reconnect is now working. It also will take care of echo in respnses and commands.

--- dragonfly/implementations/ethernet_provider.py
+++ dragonfly/implementations/ethernet_provider.py
@@ -1,3 +1,4 @@
+
 from __future__ import absolute_import
 import socket
 import threading
@@ -20,16 +21,16 @@ class EthernetProvider(Provider):
                  socket_info=("localhost",1234),
                  response_terminator = None,
                  command_terminator = None,
-                 reply_echo_cmd = False,
+                 reply_echo_cmd = True,
                  **kwargs
                  ):
         '''
-        socket_timeout (float): time in seconds for the socket to timeout
-        socket_info (tuple): (<network_address_as_str>, <port_as_int>)
-        response_terminator (str||None): string to rstrip() from responses
-        command_terminator (str||None): string to append to commands
-        reply_echo_cmd (bool): set to true if command+command_terminator are present in reply
-        '''
+            socket_timeout (float): time in seconds for the socket to timeout
+            socket_info (tuple): (<network_address_as_str>, <port_as_int>)
+            response_terminator (str||None): string to rstrip() from responses
+            command_terminator (str||None): string to append to commands
+            reply_echo_cmd (bool): set to true if command+command_terminator are present in reply
+            '''
         Provider.__init__(self, **kwargs)
         self.alock = threading.Lock()
         self.socket_timeout = float(socket_timeout)
@@ -45,6 +46,21 @@ class EthernetProvider(Provider):
             self.socket_info = (ip,int(port))
         logger.debug('socket info is {}'.format(self.socket_info))
         self.reconnect()
+    
+    def send_commands(self, commands):
+        all_data=[]
+        
+        for command in commands:
+            logger.debug('sending: {}'.format(repr(command)))
+            if self.command_terminator is not None:
+                command += self.command_terminator
+            self.socket.send(command)
+            data = self.get()
+            if (data.startswith(command) and self.reply_echo_cmd):
+                data = data[0:data.find(self.command_terminator)+len(self.command_terminator)] + data[data.rfind(self.command_terminator)+len(self.command_terminator):]
+            logger.debug('sync: {} -> {}'.format(repr(command),repr(data)))
+            all_data.append(data)
+        return all_data
 
     def reconnect(self):
         self.socket.close()
@@ -56,45 +72,47 @@ class EthernetProvider(Provider):
             raise
         self.socket.settimeout(self.socket_timeout)
         self.send("")
-
+    
     def send(self, commands):
         '''
-        this issues commands
-        '''
+            this issues commands
+            '''
         if isinstance(commands, types.StringType):
             commands = [commands]
         all_data = []
         self.alock.acquire()
         try:
-            for command in commands:
-                logger.debug('sending: {}'.format(repr(command)))
-                if self.command_terminator is not None:
-                    command += self.command_terminator
-                self.socket.send(command)
-                data = self.get()
-                if (data.startswith(command) and self.reply_echo_cmd):
-                    data = data[data.startswith(command) and len(command):]
-                logger.debug('sync: {} -> {}'.format(repr(command),repr(data)))
-                all_data.append(data)
+            all_data += self.send_commands(commands)
+
+        except socket.error:
+            self.alock.release()
+            self.reconnect()
+            self.alock.acquire()
+            all_data += self.send_commands(commands)
+
         finally:
             self.alock.release()
         to_return = ';'.join(all_data)
         logger.debug('should return:\n{}'.format(to_return))
         return to_return
-
+    
     def get(self):
         data = ""
         try:
             while True:
                 data += self.socket.recv(1024)
                 if (self.response_terminator and  data.endswith(self.response_terminator)):
+                    if(self.reply_echo_cmd):
+                        data= data[0:data.find(self.response_terminator)]
                     break
         except socket.timeout:
-            pass
+            logger.critical('Cannot Connect!')
         if self.response_terminator:
             data = data.rsplit(self.response_terminator,1)[0]
         return data
-
+    
     @property
     def spimes(self):
         return self.endpoints
+
+
