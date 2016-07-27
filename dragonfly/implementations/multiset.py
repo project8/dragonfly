ffb118e2fa0e06020a593667fe9b8d265d637e8a
guiguem <mathieu.guigue@pnnl.gov>
2016-07-27 11:52:16 -0700

    Working version of the multiset. After serving the config files in the example, one can set all the different values of the endpoints defined in "targets" except the one which have a "default_set" which is then used instead of the value given in the command line. When done, the set returns a 'done'. The next set should be to merge the multiget and the multiset in one single class, so we can access/get/set one single endpoint within the same service.

--- dragonfly/implementations/multiset.py
+++ dragonfly/implementations/multiset.py
@@ -23,70 +23,35 @@ class MultiSet(dripline.core.Endpoint):
         dripline.core.Endpoint.__init__(self, **kwargs)
 
         self._targets = []
-        self._list_flag=[]
-        #for a_name,details in targets:
-        these_details = {}
         for a_target in targets:
-            print(a_target['target'])
-
+            these_details = {}
             if 'default_set' in a_target:
                 these_details.update({'default_set':a_target['default_set']})
-
-            if 'format_target' in a_target:
-                these_details.update({'format_target':a_target['format_target']})
-            # if 'units' in a_target and 'formatter' in a_target:
-            #     raise dripline.core.DriplineValueError('may not specify both "units" and "formatter"')
-            # if 'formatter' in a_target:
-            #     these_details['formatter'] = a_target['formatter']
-            # elif 'units' in a_target:
-            #     these_details['formatter'] = '{} -> {} [{}]'.format(a_target['target'], '{}', a_target['units'])
-            # else:
-            #     these_details['formatter'] = '{} -> {}'.format(a_target['target'], '{}')
             self._targets.append([a_target['target'], these_details])
 
-        self._request_message = dripline.core.RequestMessage(msgop=dripline.core.OP_SET)
 
-    def update_parser(self,parser):
-        for flag in parser_new_flags:
-            if isinstance(flag,str):
-                parser.add_argument(flag)
-            self._list_flag.append(flag)
-        # parser.add_argument('value_to_set')
-
-    def on_set(self, *args, **kwargs):
+    def on_set(self, value):
 
         to_be_sent = []
-        # for a_target,details in self._targets:
-        #     if 'default_set' in details:
-        #         value = details['default_set']
-        #     else:
-        #         value = args[0]
-        #     # print(value)
-        #     if 'format_target' in details:
-        #         target = a_target.format(details['format_target'])
-        #     else:
-        #         target = a_target
-        #     print(value,target)
+        for a_target,details in self._targets:
+            if 'default_set' in details:
+                value_to_set = details['default_set']
+            else:
+                value_to_set = value
 
-            # result = self._single_set(a_target, value_to_set)
-            # if result.retcode !=0:
-            #     logger.warning('unable to set <{}>'.format(a_target['target']))
-            #     raise dripline.core.exception_map[result.retcode](result.return_msg)
+            result = self._single_set(a_target, value_to_set)
+            if result.retcode !=0:
+                logger.warning('unable to set <{}>'.format(a_target))
+                raise dripline.core.exception_map[result.retcode](result.return_msg)
 
-        # return {'value_raw': result_vals, 'value_cal': '\n'.join(result_reps)}
+        return 'done'
 
-    def _single_set(self, endpoint_name, details):
+    def _single_set(self, endpoint_name, value):
         '''
         attempt to set a single endpoint
         '''
         ret_val = None
         ret_rep = ''
-        a_result = self.portal.send_request(request=self._request_message, target=endpoint_name)
-        if a_result.retcode != 0:
-            ret_val = None
-            ret_rep = '{} -> returned error <{}>:{}'.format(endpoint_name, a_result.retcode, a_result.return_msg)
-        else:
-            ret_val = a_result.payload[details['payload_field']]
-            ret_rep = details['formatter'].format(ret_val)
+        a_result = self.portal.send_request(request=dripline.core.RequestMessage(msgop=dripline.core.OP_SET, payload={'values':[value]}), target=endpoint_name)
 
-        return ret_val,ret_rep
+        return a_result
