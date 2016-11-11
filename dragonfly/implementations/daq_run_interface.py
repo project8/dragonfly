03f2fdd8dc0664ed766729be7bf84e14130f0bd8
les67 <luis.saldana@yale.edu>
2016-11-11 17:27:02 -0500

    removing logging the snap payloads; this is too verbose (specially considering runs that last over an hour) and make the log of the daq service hard to sift through. logging the meta payload is still kept however

--- dragonfly/implementations/daq_run_interface.py
+++ dragonfly/implementations/daq_run_interface.py
@@ -108,7 +108,6 @@ class DAQProvider(core.Provider):
         self._do_postrun_gets()
         if not self._debug_without_snapshot_broadcast:
             self._send_snapshot(snap_flag='post')
-            logger.debug('this snapshot will be {}'.format(self._postrun_snapshot))
         run_was = self.run_id
         if self._stop_handle is not None:
             self.service._connection.remove_timeout(self._stop_handle)
@@ -130,7 +129,6 @@ class DAQProvider(core.Provider):
         if not self._debug_without_snapshot_broadcast:
             self._send_snapshot(snap_flag='pre')
         logger.debug('these meta will be {}'.format(self._run_meta))
-        logger.debug('this snapshot will be {}'.format(self._prerun_snapshot))
         logger.info('start_run finished')
 
     def _do_prerun_gets(self):
