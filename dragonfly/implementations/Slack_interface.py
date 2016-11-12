651b1f1adebdfc9c4cc8d29eb17688397e9f851b
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2016-11-11 18:56:52 -0800

    Preliminary version of the noise_reducer in the SlackInterface: need a spammer for complete test now

--- dragonfly/implementations/Slack_interface.py
+++ dragonfly/implementations/Slack_interface.py
@@ -8,6 +8,7 @@ from __future__ import absolute_import
 import json
 import os
 import re
+from datetime import datetime, timedelta
 
 import slackclient
 
@@ -21,21 +22,29 @@ class SlackInterface(Gogol):
     '''
     A generic service that will repeat AMQP messages to messages on slack.
     '''
-    def __init__(self, **kwargs):
+    def __init__(self,
+                 prime_speakers = None,
+                 speaking_time = 60,
+                 time_between_warnings=600,
+                 number_sentence_per_speaking_time = 30,
+                 **kwargs):
+        '''
+        prime_speakers: define which users are allowed to speak as much as they want and we are not allowed to stop them from it
+        '''
         # listen to status_message alerts channel
         kwargs.update({'exchange':'alerts','keys':['status_message.#.#']})
-        # kwargs.update({'exchange':'alerts','keys':['#']})
         Gogol.__init__(self, **kwargs)
 
+        # get the authentification file and look for a slack field
         this_home = os.path.expanduser('~')
         slack = {}
-        # slack = json.loads(open(this_home+'/.project8_authentications.json').read())['slack']
         config_file = json.loads(open(this_home+'/.project8_authentications_project8.json').read())
         if 'slack' in config_file:
             slack = config_file['slack']
         else:
             raise dripline.core.exceptions.DriplineValueError('Warning! unable to find slack credentials in <~/.project8_authentications.p8>')
-        print(slack)
+
+        # grab the token used for authentification to slack
         token = None
         if 'dripline' in slack:
             token = slack['dripline']
@@ -47,40 +56,78 @@ class SlackInterface(Gogol):
             self.slackclient = None
             raise dripline.core.exceptions.DriplineValueError('Warning! unable to find slack token in <~/.project8_authentications.p8>')
 
-        # this_home = os.path.expanduser('~')
-        # _tokens = json.loads(open(this_home+'/.project8_authentications.json').read())['slack']
-        # self._slackclients = {}
-        # for bot,token in _tokens.items():
-        #     self._slackclients[bot] = slackclient.SlackClient(token)
+        # Keep track of the frequency of emission of the services
+        self.history = {}
+        self._prime_speakers = prime_speakers
+        self._speaking_time = speaking_time
+        self._time_between_warnings = time_between_warnings
+        self._nspst = number_sentence_per_speaking_time
+
 
     def on_alert_message(self, channel, method, properties, message):
         # parse the routing key
         logger.debug('parsing routing key')
-        # print(message)
         key_parser = r'status_message.(?P<channel>[\w]+).(?P<from>[\w]+)'
         routing_info = re.search(key_parser, method.routing_key).groupdict()
-        #
         # parse the message
         msg = dripline.core.Message.from_encoded(message, properties.content_encoding)
-        # print(msg)
-        logger.debug('selecting client')
-        as_user = 'false' # allows to send messages with unregistred username (like toto) in the channel
-        #???????
-        # if routing_info['from'] in self._slackclients:
-        #     this_client = self._slackclients[routing_info['from']]
-        #     as_user = 'true'
-        # elif 'token' in self._slackclients:
-        #     this_client = self._slackclients['token']
-        # else:
-        #     this_client = self._slackclients['dripline']
-        # ????????
-        logger.debug('posting message')
-        api_out = self.slackclient.api_call('chat.postMessage',
-                                    #    channel='#'+routing_info['channel'],
-                                       channel='#slack_test',
-                                       text=str(msg.payload),
-                                       username=routing_info['from'],
-                                    #    username='toto',
-                                       as_user=as_user,
-                                      )
-        logger.debug('api call returned:{}'.format(api_out))
+
+        self._update_history(routing_info['from'])
+        username = routing_info['from']
+        if self._is_allowed_to_talk(routing_info['from']):
+            logger.debug('posting message: {}'.format(str(msg.payload)))
+            api_out = self.slackclient.api_call('chat.postMessage',
+                                        #    channel='#'+routing_info['channel'],
+                                           channel='#slack_test',
+                                           text=str(msg.payload),
+                                           username=username,
+                                        #    username='toto',
+                                           as_user='false', #false allows to send messages with unregistred username (like toto) in the channel
+                                          )
+            logger.debug('api call returned:{}'.format(api_out))
+            self.history[username]['last_talks'].append(datetime.now())
+        else:
+            if self.history[username]['last_warning'] is 'never' or (datetime.now()-self.history[username]['last_warning']).seconds > self._time_between_warnings:
+                logger.debug('sending warning')
+                message = '{} spoke x times over the last x s: I muted it!'.format(routing_info['from'])
+                api_out = self.slackclient.api_call('chat.postMessage',
+                                                #    channel='#'+routing_info['channel'],
+                                                   channel='#slack_test',
+                                                   text=message,
+                                                   username='Security',
+                                                #    username='toto',
+                                                   as_user='false', #false allows to send messages with unregistred username (like toto) in the channel
+                                                  )
+                logger.debug('api call returned:{}'.format(api_out))
+                self.history[username]['last_warning'] = datetime.now()
+            else:
+                logger.debug('{} was warned recently: not sending warning'.format(routing_info['from']))
+
+    def _update_history(self,username):
+        if username not in self.history:
+            self.history.update({username:{}})
+            self.history[username].update({'last_talks':[]})
+            self.history[username].update({'last_warning':'never'})
+        now = datetime.now()
+        for time in self.history[username]['last_talks']:
+            deltat = now - time
+            if deltat.seconds > self._speaking_time:
+                self.history[username].remove(time)
+        print(self.history)
+
+    def _is_allowed_to_talk(self,username):
+
+        # return true for allowed talk
+        # return false for unallowed
+        if username in self._prime_speakers:
+            is_allowed = True
+        else:
+            # print(self.history[username]['last_talks'].size())
+            if self.history[username]['last_talks'] == [] or len(self.history[username]['last_talks']) < self._nspst:
+                is_allowed = True
+
+            else:
+                logger.info('{} user not allowed to talk: talk {} over the last {} s'.format(username, len(self.history[username]['last_talks']), self._speaking_time))
+                is_allowed = False
+
+        return is_allowed
--- dragonfly/status_log_handlers/amqp_handler.py
+++ dragonfly/status_log_handlers/amqp_handler.py
@@ -37,25 +37,6 @@ class AMQPHandler(logging.Handler):
         print('sending to alerts exchange with severity {} message ({})'.format(severity,'hello world'))
         self.connection_to_alert.send_status_message(severity=severity,alert='hello world')
 
-
-        # this_home = os.path.expanduser('~')
-        # slack = {}
-        # try:
-        #     slack = json.loads(open(this_home+'/.project8_authentications.json').read())['slack']
-        # except:
-        #     print('either unable to read ~/.project8_authentications.json or no slack field configured')
-        #     #logger.warning('unable to parse authentication file')
-        # token = None
-        # if 'dripline' in slack:
-        #     token = slack['dripline']
-        # elif 'token' in slack:
-        #     token = slack['token']
-        # if token:
-        #     self.slackclient = slackclient.SlackClient(token)
-        # else:
-        #     self.slackclient = None
-        #     print('\nWarning! unable to find slack credentials in <~/.project8_authentications.p8>\n')
-
     def update_parser(self, parser):
         parser.add_argument('--'+self.argparse_flag_str,
                             action='store_false',
