38394146bbedb0c06e94e42e0bd5cb750e2bd0eb
Walter C. Pettus <pettus@uw.edu>
2016-12-20 14:33:41 -0800

    cleaning multido functionality out of esr

--- dragonfly/implementations/esr_measurement.py
+++ dragonfly/implementations/esr_measurement.py
@@ -23,44 +23,16 @@ class ESR_Measurement(core.Endpoint):
     def __init__(self,
                  lockin_n_points,
                  lockin_sampling_interval,
-                 lockin_trigger,
-                 lockin_curve_mask,
-                 lockin_srq_mask,
-                 lockin_osc_amp,
-                 lockin_osc_freq,
-                 lockin_ac_gain,
-                 lockin_sensitivity,
-                 lockin_time_constant,
-                 hf_sweep_order,
-                 hf_start_freq,
-                 hf_stop_freq,
-                 hf_power,
-                 hf_n_sweep_points,
-                 hf_dwell_time,
                  **kwargs):
         core.Endpoint.__init__(self,**kwargs)
         # Settings for lockin and sweeper
-        self.lockin_n_points = self._default_lockin_n_points = lockin_n_points
-        self.lockin_sampling_interval = self._default_lockin_sampling_interval = lockin_sampling_interval
-        self.lockin_trigger = self._default_lockin_trigger = lockin_trigger
-        self.lockin_curve_mask = self._default_lockin_curve_mask = lockin_curve_mask
-        self.lockin_srq_mask = self._default_lockin_srq_mask = lockin_srq_mask
-        self.lockin_osc_amp = self._default_lockin_osc_amp = lockin_osc_amp
-        self.lockin_osc_freq = self._default_lockin_osc_freq = lockin_osc_freq
-        self.lockin_ac_gain = self._default_lockin_ac_gain = lockin_ac_gain
-        self.lockin_sensitivity = self._default_lockin_sensitivity = lockin_sensitivity
-        self.lockin_time_constant = self._default_lockin_time_constant = lockin_time_constant
-        self.hf_sweep_order = self._default_hf_sweep_order = hf_sweep_order
-        self.hf_start_freq = self._default_hf_start_freq = hf_start_freq
-        self.hf_stop_freq = self._default_hf_stop_freq = hf_stop_freq
-        self.hf_power = self._default_hf_power = hf_power
-        self.hf_n_sweep_points = self._default_hf_n_sweep_points = hf_n_sweep_points
-        self.hf_dwell_time = self._default_hf_dwell_time = hf_dwell_time
+        self.lockin_n_points = None
+        self.lockin_sampling_interval = None
         # Output storage
         self.output_dict = {}
 
 
-    def run_scan(self, config_instruments=True, restore_defaults=True, coils=[1,2,3,4,5], n_fits=2, **kwargs):
+    def run_scan(self, config_instruments=True, coils=[1,2,3,4,5], n_fits=2, **kwargs):
         '''
         Wraps all ESR functionality into single method call with tunable arguments.
         This function should interface with run_scripting through the action_esr_run method.
@@ -79,67 +51,19 @@ class ESR_Measurement(core.Endpoint):
         return "ESR data file created: {}".format(outfile)
 
 
-    def configure_instruments(self, reset):
+    def configure_instruments(self):
         '''
         Configure instruments to default settings.
         '''
-        if reset:
-            self.restore_presets()
-        # lockin controls
-        self.check_endpoint('lockin_n_points', self.lockin_n_points)
-        self.check_endpoint('lockin_sampling_interval', self.lockin_sampling_interval)
-        self.check_endpoint('lockin_trigger', self.lockin_trigger)
-        self.check_endpoint('lockin_curve_mask', self.lockin_curve_mask)
-        self.check_endpoint('lockin_srq_mask', self.lockin_srq_mask)
-        self.check_endpoint('lockin_osc_amp', self.lockin_osc_amp)
-        self.check_endpoint('lockin_osc_freq', self.lockin_osc_freq)
-        #self.check_endpoint('lockin_ac_gain', self.lockin_ac_gain) # Lockin settings currently bind ACGAIN to SEN parameter
-        self.check_endpoint('lockin_sensitivity', self.lockin_sensitivity)
-        self.check_endpoint('lockin_time_constant', self.lockin_time_constant)
-        # sweeper controls
-        while True:
-            err_msg = self.raw_get_endpoint('hf_error_check')
-            if err_msg == '+0,"No error"':
-                break
-            logger.warning("Clearing sweeper error queue: {}".format(err_msg))
-        self.check_endpoint('hf_output_status', 0)
-        self.check_endpoint('hf_freq_mode', 'LIST')
-        self.check_endpoint('hf_sweep_order', str(self.hf_sweep_order))
-        self.check_endpoint('hf_start_freq', float(self.hf_start_freq))
-        self.check_endpoint('hf_stop_freq', float(self.hf_stop_freq))
-        self.check_endpoint('hf_power', self.hf_power)
-        self.check_endpoint('hf_n_sweep_points', self.hf_n_sweep_points)
-        self.check_endpoint('hf_dwell_time', self.hf_dwell_time)
+        self.provider.set('lockin_esr_settings',1)
         err_msg = self.raw_get_endpoint('hf_error_check')
-        if err_msg != '+0,"No error"':
-            raise core.exceptions.DriplineHardwareError("Sweeper error: {}".format(err_msg))
+        if not err_msg ==  '+0,"No error"':
+            logger.warning("Clearing sweeper error queue: {}".format(err_msg))
+        self.provider.set('sweeper_esr_settings',1)
         # relays
         self.check_endpoint('esr_tickler_switch', 1)
         for coil in range(1, 6):
             self.check_endpoint('esr_coil_{}_switch_status'.format(coil), 0)
-
-    def restore_presets(self):
-        '''
-        Reset all internal variables to presets loaded from config
-        '''
-        # lockin presets
-        self.lockin_n_points = self._default_lockin_n_points
-        self.lockin_sampling_interval = self._default_lockin_sampling_interval
-        self.lockin_trigger = self._default_lockin_trigger
-        self.lockin_curve_mask = self._default_lockin_curve_mask
-        self.lockin_srq_mask = self._default_lockin_srq_mask
-        self.lockin_osc_amp = self._default_lockin_osc_amp
-        self.lockin_osc_freq = self._default_lockin_osc_freq
-        self.lockin_ac_gain = self._default_lockin_ac_gain
-        self.lockin_sensitivity = self._default_lockin_sensitivity
-        self.lockin_time_constant = self._default_lockin_time_constant
-        # sweeper presets
-        self.hf_sweep_order = self._default_hf_sweep_order
-        self.hf_start_freq = self._default_hf_start_freq
-        self.hf_stop_freq = self._default_hf_stop_freq
-        self.hf_power = self._default_hf_power
-        self.hf_n_sweep_points = self._default_hf_n_sweep_points
-        self.hf_dwell_time = self._default_hf_dwell_time
 
     def reset_configure(self):
         '''
@@ -160,8 +84,8 @@ class ESR_Measurement(core.Endpoint):
         settings['insert'] = self.raw_get_endpoint("run_metadata", timeout=20)
         settings['lockin'] = self.raw_get_endpoint("lockin_settings", timeout=20)
         settings['sweeper'] = self.raw_get_endpoint("sweeper_settings")
-        self.lockin_n_points = settings['lockin']['lockin_n_points']
-        self.lockin_sampling_interval = settings['lockin']['lockin_sampling_interval']
+        self.lockin_n_points = int(settings['lockin']['lockin_n_points'])
+        self.lockin_sampling_interval = int(settings['lockin']['lockin_sampling_interval'])
         self.output_dict.update({'header' : settings})
 
     def single_measure(self, coil):
