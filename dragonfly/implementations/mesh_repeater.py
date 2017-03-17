6c614b842f6636d6be8c080c3e48786a60c80ca9
Ben LaRoque <benjamin.laroque@gmail.com>
2017-03-17 13:19:46 -0700

    initial implementation

--- dragonfly/implementations/mesh_repeater.py
+++ dragonfly/implementations/mesh_repeater.py
@@ -4,12 +4,13 @@ allowing a *one directional* link between otherwise independent meshes.
 '''
 from __future__ import absolute import
 
-from dripline.core import Provider
+from dripline.core import Provider, Interface
 
 import logging
 logger=logging.getLogger(__name__)
 
 __all__ = ['MeshRepeater',
+           'ProxyEndpoint',
           ]
 
 class MeshRepeater(Provider):
@@ -18,28 +19,22 @@ class MeshRepeater(Provider):
     It provides a communication link, but does not do any communication itself.
     Instances of ProxyEndpoint can be added to bind targets on the local mesh to the remote mesh. (see ProxyEndpoint for more details)
     '''
-    def __init__(self, target_broker, target_user, target_password, **kwargs):
+    def __init__(self, target_broker, **kwargs):
         '''
-        target_broker: (str) network-resolvable path to the broker to which requests are relayed
-        target_user: (str) username used for connecting to the target mesh
-        target_password: (str) password used for connecting to the target mesh
+        target_broker (str): network-resolvable path to the broker to which requests are relayed
+        target_user (str): username used for connecting to the target mesh
+        target_password (str): password used for connecting to the target mesh
         '''
         Provider.__init(**kwargs):
-        self._mesh_credentials = {'host': target_broker,
-                                  'username': target_user,
-                                  'password': target_password,
-                                 }
-        # create a(n Interface?) instance (service? what exists now...) for use
-        #self._interface = <something here>
+        self._interface = Interface(amqp_url, name=self.name+'_client', broker=target_broker, exchange='requests')
 
-    def send_request(self, target, request):
+    def forward_request(self, target, request):
         '''send a request message to a target in the remote mesh
 
-        target: (str) full routing key to which request is sent
-        request: (RequestMessage) dripline.core.RequestMessage to send to target
+        target (str): full routing key to which request is sent
+        request (RequestMessage): dripline.core.RequestMessage to send to target
         '''
-        # use self._interface to send <request> to <target>, return the result
-        pass
+        self._interface.send_request(target, request)
 
 
 
@@ -47,13 +42,11 @@ class ProxyEndpoint(Endpoint):
     '''
     Endpoint which responds to *all* RequestMessages received by passing them to the configured target via self.proivder (expected to be a MeshRepeater).
     '''
-    # design question: should this class blindly send literally *all* requests to its target?
-    # if so that's easy, just override handle_request to send the message
-    # alternately, the core methods (such as lock and ping) could apply to the proxy itself, not the target.
-    #... I have no idea which behavior represents "least surprise" for a reasonable user
-    #(the service itself will still support locking and such... perhaps I start with the first option)
 
     def __init__(self, target, **kwargs):
+        '''
+        target: (str) name of the endpoint in the remote mesh, to which requests will be sent.
+        '''
         Endpoint.__init__(self, **kwargs)
         self._target = target
 
@@ -62,4 +55,4 @@ class ProxyEndpoint(Endpoint):
         # we could easily apply arbitrarly complex logic here if we wish to do so,
         # but it is not currently clear what that could/should be.
         # There is a certain cleanness to the current version.
-        self.provider.send_request(method.routing_key.(self.name, self._target, 1)
+        self.provider.forward_request(method.routing_key.(self.name, self._target, 1)
