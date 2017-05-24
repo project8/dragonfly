c8d0a720dc09a725eb67d5dd7989bc4a6333c244
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2017-05-24 15:11:18 -0700

    some cleaning and documentation

--- dragonfly/implementations/pinger.py
+++ dragonfly/implementations/pinger.py
@@ -18,38 +18,35 @@ from time import sleep
 __all__ = []
 __all__.append('Pinger')
 
-class Pinger(Scheduler,Endpoint):
+class Pinger(Endpoint,Scheduler):
     '''
     Ping on a regular basis a list of service.
     It sends a request to target.ping, if a response is not given within the timeout,
     it reports a logger.critical, which sends a alert to status_message.critical.<service_name>
     via the AMQPHandler.
+    services_to_ping: list of the services to ping
+    ping_timeout: duration before considering a ping failed
     '''
     def __init__(self,
                  broker=None,
-                 ping_interval = 60,
                  services_to_ping = [],
                  ping_timeout = 10,
                  *args, **kwargs):
 
-
         Endpoint.__init__(self,**kwargs)
-
         Scheduler.__init__(self, **kwargs)
-        
-        self.ping_interval = ping_interval
+
         self.services_to_ping = services_to_ping
         self.ping_timeout = ping_timeout
 
-        # logger.info('sleep before starting pinging the services')
-        # sleep(self.ping_interval)
-
+    # FIXME: the following method is called by Scheduler on a regular basis.
+    # Should be replaced with a more generic method name.
     def _log_a_value(self):
         message = ""
         for item in self.services_to_ping:
             logger.info("pinging {}".format(item))
             try:
-                result = self.provider.cmd(target=str(item), method_name="ping", value=[], timeout=self.ping_interval)
+                result = self.provider.cmd(target=str(item), method_name="ping", value=[], timeout=self.ping_timeout)
                 if result:
                     logger.info("{} is responding".format(item))
             except Exception as err:
@@ -57,3 +54,6 @@ class Pinger(Scheduler,Endpoint):
                 message = message + "{}\n".format(item)
         if message != "":
             logger.critical("The following services are not responding:\n{}".format(message))
+
+        # start to sleep before restarting
+        self._timeout_handle = self.service._connection.add_timeout(self._log_interval, self._log_a_value)
