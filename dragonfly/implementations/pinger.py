1c255e8e41b997808b653a81585902ce1dca7f27
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2017-05-24 13:28:26 -0700

    not working

--- dragonfly/implementations/pinger.py
+++ dragonfly/implementations/pinger.py
@@ -11,14 +11,14 @@ import logging
 logger=logging.getLogger(__name__)
 
 import dripline
-from dripline.core import Endpoint
+from dripline.core import Endpoint, Scheduler
 
 from time import sleep
 
 __all__ = []
 __all__.append('Pinger')
 
-class Pinger(Endpoint):
+class Pinger(Scheduler,Endpoint):
     '''
     Ping on a regular basis a list of service.
     It sends a request to target.ping, if a response is not given within the timeout,
@@ -32,28 +32,28 @@ class Pinger(Endpoint):
                  ping_timeout = 10,
                  *args, **kwargs):
 
+
         Endpoint.__init__(self,**kwargs)
 
+        Scheduler.__init__(self, **kwargs)
+        
         self.ping_interval = ping_interval
         self.services_to_ping = services_to_ping
         self.ping_timeout = ping_timeout
 
-        logger.info('sleep before starting pinging the services')
-        sleep(self.ping_interval)
+        # logger.info('sleep before starting pinging the services')
+        # sleep(self.ping_interval)
 
-
-    def start_ping(self):
-        while (True):
-            message = ""
-            for item in self.services_to_ping:
-                logger.info("pinging {}".format(item))
-                try:
-                    result = self.provider.cmd(target=str(item), method_name="ping", value=[], timeout=self.ping_interval)
-                    if result:
-                        logger.info("{} is responding".format(item))
-                except Exception as err:
-                    logger.info(err)
-                    message = message + "{}\n".format(item)
-            if message != "":
-                logger.critical("The following services are not responding:\n{}".format(message))
-            sleep(self.ping_interval)
+    def _log_a_value(self):
+        message = ""
+        for item in self.services_to_ping:
+            logger.info("pinging {}".format(item))
+            try:
+                result = self.provider.cmd(target=str(item), method_name="ping", value=[], timeout=self.ping_interval)
+                if result:
+                    logger.info("{} is responding".format(item))
+            except Exception as err:
+                logger.info(err)
+                message = message + "{}\n".format(item)
+        if message != "":
+            logger.critical("The following services are not responding:\n{}".format(message))
