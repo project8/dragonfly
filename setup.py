f9d50d30b20640f8b0967d6926093919afe80283
Walter C. Pettus <pettus@uw.edu>
2017-08-08 17:34:48 -0700

    Working version of MAX31856 readout via RPi GPIO

--- setup.py
+++ setup.py
@@ -20,8 +20,9 @@ extras_require={
     'database': ['psycopg2', 'sqlalchemy'], #this may also require system packages
     'debug': ['ipdb'],
     'doc': ['sphinx', 'sphinx_rtd_theme', 'sphinxcontrib-programoutput'],
+    'max31856': ['adafruit_max31856','rpi.gpio'], #only for RPi
     'roach': ['corr==0.7.3','katcp==0.5.5','construct==2.5.2','scipy==0.19.0','netifaces==0.10.5','adc5g==0.0.1'],
-    'gpio': ['rpi.gpio'],
+    'gpio': ['rpi.gpio'], #only for RPi
     'slack': ['slackclient']
 }
 everything = set()
@@ -35,7 +36,8 @@ setup(
     packages=['dragonfly','dragonfly/implementations','dragonfly/status_log_handlers','dragonfly/subcommands'],
     scripts=['bin/dragonfly'],
     install_requires=['dripline', 'asteval'],
-    dependency_links=['git+https://github.com/sma-wideband/adc_tests.git@65a2ef4e1cf68bee35176a1171d923a73952e13e#egg=adc5g-0.0.1'],
+    dependency_links=['git+https://github.com/sma-wideband/adc_tests.git@65a2ef4e1cf68bee35176a1171d923a73952e13e#egg=adc5g-0.0.1',
+                      'git+https://github.com/johnrbnsn/Adafruit_Python_MAX31856.git#egg=adafruit_max31856-0.0.1'],
     extras_require=extras_require,
     url='http://www.github.com/project8/dragonfly',
 )
