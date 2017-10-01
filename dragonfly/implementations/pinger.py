6c92d975996d0e286880a20ac13c530417b6ff83
Walter C. Pettus <pettus@uw.edu>
2017-10-01 15:51:18 -0700

    Adding get alarm_status for sensor_monitor, and adding silence functionality to pinger and sensor_monitor

--- dragonfly/implementations/pinger.py
+++ dragonfly/implementations/pinger.py
@@ -4,25 +4,21 @@ Ping on a regular basis a list of endpoints
 
 from __future__ import absolute_import
 
-import json
-import os
+import datetime
 
 import logging
 logger=logging.getLogger(__name__)
 
-import dripline
-from dripline.core import Endpoint, Scheduler
-
-from time import sleep
+from dripline.core import Endpoint, Scheduler, constants, exceptions, fancy_doc
 
 __all__ = []
 __all__.append('Pinger')
-
+@fancy_doc
 class Pinger(Endpoint,Scheduler):
     '''
     Ping on a regular basis a list of service.
     It sends a request to target.ping, if a response is not given within the timeout,
-    it reports a logger.critical, which sends a alert to status_message.critical.<service_name>
+    it reports a logger.critical, which sends an alert to status_message.critical.<service_name>
     via the AMQPHandler.
     services_to_ping: list of the services to ping
     ping_timeout: duration before considering a ping failed
@@ -37,6 +33,7 @@ class Pinger(Endpoint,Scheduler):
 
         self.services_to_ping = services_to_ping
         self.ping_timeout = ping_timeout
+        self.silenced_pingers = {}
 
     def scheduled_action(self):
         '''
@@ -44,6 +41,13 @@ class Pinger(Endpoint,Scheduler):
         '''
         message = ""
         for item in self.services_to_ping:
+            if item in self.silenced_pingers:
+                if self.silenced_pingers[item] > datetime.datetime.utcnow():
+                    logger.debug("skipping ping of {} until {}".format(item, self.silenced_pingers[item]))
+                    continue
+                else:
+                    logger.debug("reactivating pinger of {}".format(item))
+                    self.silenced_pingers.pop(item, None)
             logger.debug("pinging {}".format(item))
             try:
                 result = self.provider.cmd(target=item, method_name="ping", value=[], timeout=self.ping_timeout)
@@ -54,3 +58,15 @@ class Pinger(Endpoint,Scheduler):
                 message = message + "{}\n".format(item)
         if message != "":
             logger.critical("The following services are not responding:\n{}".format(message))
+
+    def silence_ping(self, service, endtime):
+        if service not in self.services_to_ping:
+            raise exceptions.DriplineValueError("Invalid service <{}>, not found in {}".format(service, self.services_to_ping))
+        enddatetime = datetime.datetime.strptime(endtime,constants.TIME_FORMAT)
+        if enddatetime < datetime.datetime.utcnow():
+            self.silenced_pingers.pop(service, None)
+            return "Ignoring endtime in the past.  Pinger active!"
+        if enddatetime > datetime.datetime.utcnow()+datetime.timedelta(1):
+            raise exceptions.DriplineValueError("Invalid endtime provided <{}>, one day maximum silence interval".format(endtime))
+        self.silenced_pingers.update( { service : enddatetime } )
+        logger.warning('Silencing pinger for {} until {}'.format(service, endtime))
--- dragonfly/implementations/sensor_monitor.py
+++ dragonfly/implementations/sensor_monitor.py
@@ -29,7 +29,7 @@ class SensorMonitor(Gogol):
 
     def __init__(self,
                  sensors,
-                 key_base='sensor_value.',
+                 routing_key_base='sensor_value.',
                  **kwargs
                 ):
         '''
@@ -38,8 +38,32 @@ class SensorMonitor(Gogol):
         '''
         self.start_time = datetime.datetime.utcnow()
         self.configure_monitors(sensors)
-        kwargs.update({'keys':[key_base+x for x in self.monitors.keys()]})
+        kwargs.update({'keys':[routing_key_base+x for x in self.monitors.keys()]})
         Gogol.__init__(self, **kwargs)
+
+
+    @property
+    def alarm_status(self):
+        status = { x : { y : self.monitors[x][y] for y in self.monitors[x] if y not in ('alarms') } for x in self.monitors }
+        return { 'value_raw' : status,
+                 'value_cal' : '\n'.join(['{} : {}'.format(key,status[key]) for key in status]) }
+
+
+    def silence_alarm(self, target, alarm_ct, endtime):
+        if target not in self.monitors:
+            raise exceptions.DriplineValueError("Invalid target <{}>, not found in {}".format(target, self.monitors.keys()))
+        if alarm_ct not in range(len(self.monitors[target]['alarms'])):
+            raise exceptions.DriplineValueError("Invalid alarm_ct provided <{}>, target only has {} alarms configured".format(endtime, len(self.monitors[target]['alarms'])))
+        enddatetime = datetime.datetime.strptime(endtime,constants.TIME_FORMAT)
+        if enddatetime < datetime.datetime.utcnow():
+            self.monitors[target]['alarms'][alarm_ct].pop('silenced', None)
+            self.monitors[target]['status'] = None
+            return "Ignoring endtime in the past.  Alarm active!"
+        if enddatetime > datetime.datetime.utcnow()+datetime.timedelta(1):
+            raise exceptions.DriplineValueError("Invalid endtime provided <{}>, one day maximum silence interval".format(endtime))
+        self.monitors[target]['alarms'][alarm_ct].update( { 'silenced' : enddatetime } )
+        self.monitors[target]['status'] = 'SILENT'
+        logger.warning('Silencing target {} alarm #{} until {}'.format(target, alarm_ct, endtime))
 
 
     def this_consume(self, message, method):
@@ -47,20 +71,36 @@ class SensorMonitor(Gogol):
         if sensor not in self.monitors.keys():
             logger.warning('Received invalid routing key <{}> in method\n\t{}'.format(sensor,method))
             return
-        payload = message.payload
-        payload.update( { 'timestamp' : message.timestamp } )
-        if payload is None:
+        if message.payload is None:
             logger.warning('Received invalid monitor payload!')
             return
+        self.monitors[sensor].update( { 'timestamp' : message.timestamp,
+                                        'payload' : message.payload } )
         logger.info('Consuming message from {}'.format(sensor))
-        self.process_new_value(self.monitors[sensor], payload)
+        self.process_new_value(sensor)
 
 
-    def process_new_value(self, monitor, payload):
+    def process_new_value(self, sensor):
+        monitor = self.monitors[sensor]
+        payload = monitor['payload']
         logger.debug('Processing payload <{}>'.format(payload))
         alarm_list = []
         for alarm in monitor['alarms']:
-            value = payload[alarm['payload_field']]
+            if 'silenced' in alarm:
+                if datetime.datetime.utcnow() < alarm['silenced']:
+                    logger.debug('Skipping {} during silenced interval'.format(alarm))
+                    continue
+                else:
+                    alarm.pop('silenced')
+                    logger.debug('Returning alarm to active state')
+            try:
+                value = payload[alarm['payload_field']]
+                if not isinstance(value, (int,float)):
+                    logger.critical('Invalid payload for sensor <{}>: {}'.format(sensor, value))
+                    return
+            except KeyError:
+                logger.critical('Invalid payload field for sensor <{}>: {}'.format(sensor, alarm['payload_field']))
+                return
             if alarm['averaging']:
                 alarm['data_record'].append(value)
                 if not len(alarm['data_record']) < alarm['averaging']:
@@ -75,7 +115,7 @@ class SensorMonitor(Gogol):
                 logger.info('Failed min_value check: value <{}> less than limit <{}>'.format(value, alarm['min_value']))
                 alarm_list.append('min_value')
             if 'max_slope' in alarm.keys():
-                x = (datetime.datetime.strptime(payload['timestamp'],constants.TIME_FORMAT)-self.start_time).total_seconds()
+                x = (datetime.datetime.strptime(monitor['timestamp'],constants.TIME_FORMAT)-self.start_time).total_seconds()
                 alarm['timeseries'].append([x,value])
                 if len(alarm['timeseries']) < alarm['slope_points']:
                     logger.debug('Only {} events in record'.format(len(alarm['timeseries'])))
@@ -95,7 +135,12 @@ class SensorMonitor(Gogol):
 
             # If alarm condition satisfied, ignore lower-level alarms
             if len(alarm_list) != 0:
+                self.monitors[sensor]['status'] = 'ALARM'
                 return
+        if True in ['silenced' in alarm for alarm in self.monitors[sensor]]:
+            self.monitors[sensor]['status'] = 'SILENT'
+        else:
+            self.monitors[sensor]['status'] = 'OK'
 
 
     def process_alarms(self, alarm, result):
@@ -132,56 +177,58 @@ class SensorMonitor(Gogol):
 
     def configure_monitors(self, sensors):
         for monitor in sensors:
-            if 'exchange' not in monitor.keys():
-                raise exceptions.DriplineValueError('No exchange provided for {}'.format(monitor))
+            if 'target' not in monitor.keys():
+                raise exceptions.DriplineValueError('No target provided for {}'.format(monitor))
             if 'alarms' not in monitor.keys() or not isinstance(monitor['alarms'],list):
                 raise exceptions.DriplineValueError('No valid alarms provided for {}'.format(monitor))
             for alarm in monitor['alarms']:
-                self.check_monitor_fields(alarm, monitor['exchange'])
+                self.check_monitor_fields(alarm, monitor['target'])
+            monitor.update( { 'status' : None,
+                              'timestamp' : None,
+                              'payload' : None } )
 
-        self.monitors = {x.pop('exchange'):x for x in sensors}
+        self.monitors = {x.pop('target'):x for x in sensors}
         logger.debug('Configured monitors:\n{}'.format(self.monitors))
 
 
-    def check_monitor_fields(self, alarm, exchange):
+    def check_monitor_fields(self, alarm, target):
         if 'payload_field' not in alarm.keys():
-            raise exceptions.DriplineValueError('No payload_field provided for {} : {}'.format(exchange,alarm))
+            raise exceptions.DriplineValueError('No payload_field provided for {} : {}'.format(target,alarm))
         if 'alarm_message' not in alarm.keys():
-            alarm.update({'alarm_message':'{} value raised <{{}}> alarm'.format(exchange)})
+            alarm.update({'alarm_message':'{} value raised <{{}}> alarm'.format(target)})
         if 'alarm_limit' not in alarm.keys():
             alarm.update({'alarm_limit':False})
         if alarm['alarm_limit']:
             if not isinstance(alarm['alarm_limit'],int):
-                raise exceptions.DriplineValueError('Invalid alarm_limit type provided for {} : {}'.format(exchange,alarm))
+                raise exceptions.DriplineValueError('Invalid alarm_limit type provided for {} : {}'.format(target,alarm))
             alarm.update({'alarm_count':0})
         if 'alarm_recurrence' in alarm.keys():
             if not alarm['alarm_recurrence']>0 and isinstance(alarm['alarm_recurrence'],(int,float)):
-                raise exceptions.DriplineValueError('Invalid alarm_recurrence provided for {} : {}'.format(exchange,alarm))
+                raise exceptions.DriplineValueError('Invalid alarm_recurrence provided for {} : {}'.format(target,alarm))
             alarm.update({'last_alarm':self.start_time})
         if 'set_condition' in alarm.keys():
             if not isinstance(alarm['set_condition'],int):
-                raise exceptions.DriplineValueError('Invalid set_condition type provided for {} : {}'.format(exchange,alarm))
-            if alarm['alarm_limit']:
-                raise exceptions.DriplineValueError('Invalid alarm_limit setting with set_condition active for {} : {}.'.format(exchange,alarm))
+                raise exceptions.DriplineValueError('Invalid set_condition type provided for {} : {}'.format(target,alarm))
         if 'averaging' not in alarm.keys():
             alarm.update({'averaging':False})
         if alarm['averaging']:
             if not alarm['averaging']>0 and isinstance(alarm['averaging'],int):
-                raise exceptions.DriplineValueError('Invalid averaging provided for {} : {}'.format(exchange,alarm))
+                raise exceptions.DriplineValueError('Invalid averaging provided for {} : {}'.format(target,alarm))
             alarm.update({'data_record':[]})
         if 'max_value' in alarm.keys():
-            self.check_if_float('max_value',alarm,exchange)
+            self.check_if_float('max_value',alarm,target)
         if 'min_value' in alarm.keys():
-            self.check_if_float('min_value',alarm,exchange)
+            self.check_if_float('min_value',alarm,target)
         if 'max_slope' in alarm.keys():
-            self.check_if_float('max_slope',alarm,exchange)
+            self.check_if_float('max_slope',alarm,target)
             if 'slope_points' not in alarm.keys():
-                raise exceptions.DriplineValueError('Setting slope_points required with max_slope for {} : {}'.format(exchange,alarm))
+                raise exceptions.DriplineValueError('Setting slope_points required with max_slope for {} : {}'.format(target,alarm))
             alarm.update({'timeseries':[]})
 
-    def check_if_float(self,key,alarm,exchange):
+
+    def check_if_float(self,key,alarm,target):
         if not isinstance(alarm[key],(int,float)):
             try:
                 alarm[key] = float(alarm[key])
             except ValueError:
-                raise exceptions.DriplineValueError('Invalid {} provided for {} : {}'.format(key,exchange,alarm))
+                raise exceptions.DriplineValueError('Invalid {} provided for {} : {}'.format(key,target,alarm))
