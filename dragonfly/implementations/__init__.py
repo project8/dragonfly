347f11ec0eae5b5ba937ed7849cde1b29b96ea59
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2018-05-23 17:07:18 -0700

    Adding diopsid to the __init__.py

--- dragonfly/implementations/__init__.py
+++ dragonfly/implementations/__init__.py
@@ -7,6 +7,7 @@ from __future__ import absolute_import
 from .alert_spammer import *
 from .daq_db_interface import *
 from .daq_run_interface import *
+from .diopsid import *
 from .esr_measurement import *
 from .ethernet_provider import *
 from .kv_store import *
