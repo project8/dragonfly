7c5ea13ea23b21899eeb353f659775cdf6fc239c
cclaessens <claessen@uni-mainz.de>
2018-08-01 22:01:50 -0700

    added mode check to time_window_settings property

--- dragonfly/implementations/roach_daq_run_interface.py
+++ dragonfly/implementations/roach_daq_run_interface.py
@@ -488,6 +488,8 @@ class ROACH1ChAcquisitionInterface(DAQProvider):
         '''
         Returns pretrigger time and skip tolerance
         '''
+        if self.acquisition_mode is not 'triggering':
+            return False
         result = self.provider.cmd(self.psyllid_interface, 'get_time_window', payload = self.payload_channel)
         return result
 
