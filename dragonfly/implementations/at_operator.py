c2bf5e59362b4d2c60221d4e2992cfb893755d50
yadiw <32943329+yadiw@users.noreply.github.com>
2018-11-19 15:20:04 -0800

    make adjustments to implement functionalities related to monitor channel

--- dragonfly/implementations/at_operator.py
+++ dragonfly/implementations/at_operator.py
@@ -1,4 +1,4 @@
-import datetime, logging, os, time, funcsigs
+import datetime, logging, os, time, funcsigs, json
 from dateutil import parser
 # for google
 from googleapiclient.discovery import build
@@ -12,10 +12,18 @@ logger = logging.getLogger(__name__)
 
 class AtOperator():
     
-    def __init__(self):
+    def __init__(self, directory = '/home/yadiw/Desktop/Operator/bot_token.json', monitor_channel_name = 'general'):
 
         self.calendar_scope = 'https://www.googleapis.com/auth/calendar.readonly'
-        self.slack_client = SlackClient(os.environ.get('BOT_TOKEN'))
+
+        with open(directory, 'r') as load_f:
+            token_dict = json.load(load_f)
+            self.slack_client = SlackClient(token_dict["bot_token"])
+            
+        self.monitor_channel_name = monitor_channel_name
+        self.monitor_channel_id = ''
+        self.channel_name_to_id_dictionary = {}
+        self.channel_id_to_name_dictionary = {}
 
         self.current_operator_name = ''
         self.current_operator_id = ''
@@ -72,20 +80,42 @@ class AtOperator():
                     return event['summary'].replace('Operator: ', '')
         return None
 
-    def construct_dictionaries(self):
+    def construct_user_dictionaries(self):
         request = self.slack_client.api_call("users.list")
         if request['ok']:
+            full_name_to_id_dictionary = {}
+            id_to_username_dictionary = {}
+            username_to_id_dictionary = {}
             for m in request['members']:
                 if not m['is_bot']:
-                    self.full_name_to_id_dictionary[m['real_name']] = m['id']
-                    self.id_to_username_dictionary[m['id']] = m['name']
-                    self.username_to_id_dictionary[m['name']] = m['id']
+                    full_name_to_id_dictionary[m['real_name']] = m['id']
+                    id_to_username_dictionary[m['id']] = m['name']
+                    username_to_id_dictionary[m['name']] = m['id']
+            logger.info(' Constructed dictionaries for full names, Slack usernames and ids.')
+            return full_name_to_id_dictionary, id_to_username_dictionary, username_to_id_dictionary
         else:
-            logger.critical(' Cannot get the list of users!')
-            os.exit(1)
+            return None, None, None
+    
+    def construct_channel_dictionaries(self):
+        logger.info(" Trying to construct an dictionary mapping channel names to their ids.")
+        request = self.slack_client.api_call("conversations.list")
+        if request['ok']:
+            channel_name_to_id_dictionary = {}
+            channel_id_to_name_dictionary = {}
+            for c in request['channels']:
+                channel_name_to_id_dictionary[c['name']] = c['id']
+                channel_id_to_name_dictionary[c['id']] = c['name']
+            logger.info(' Constructed dictionaries containing information of ' + str(len(request['channels'])) + ' channels.')
+            return channel_name_to_id_dictionary, channel_id_to_name_dictionary
 
+        else:
+            return None, None
+
+
+    # send message to an channel where @operator or any helper command is called.
     def send_message(self, channel, text):
-        self.slack_client.api_call("chat.postMessage", channel=channel, text=text, as_user=True)
+        if channel in self.channel_id_to_name_dictionary:
+            self.slack_client.api_call("chat.postMessage", channel=channel, text=text, as_user=True)
 
     def at_operator(self, channel):
         self.send_message(channel, "Get it!")
@@ -100,6 +130,7 @@ class AtOperator():
         else:
             self.send_message(channel, "<@" + self.current_operator_id + ">")
 
+        
     def command_hello(self, channel, user_id):
         self.send_message(channel, "Hi, " + self.id_to_username_dictionary[user_id] + ".")
 
@@ -157,6 +188,7 @@ class AtOperator():
         self.command_dictionary["!whoisop"] = self.command_whoisop
         self.command_dictionary["!tempoperator"] = self.command_tempoperator
         self.command_dictionary["!removetempoperator"] = self.command_removetempoperator
+        logger.info(' Constructed dictionaries for operator helper commands on Slack.')
 
     def parse_output(self, rtm_output, bot_id):
         output = rtm_output
@@ -183,6 +215,8 @@ class AtOperator():
                         func(*args[:num_args])
         return None
 
+
+
     def run(self):
         logging.getLogger('googleapiclient.discovery_cache').setLevel(logging.ERROR)
         if self.slack_client.rtm_connect():
@@ -192,31 +226,47 @@ class AtOperator():
                 logger.critical(' Unable to get the bot user ID.')
                 os.exit(1)
             logger.info(' Got Slack Operator bot id.')
-            self.construct_dictionaries()
-            logger.info(' Constructed dictionaries for full names, Slack usernames and ids.')
-            self.construct_command_dictionary()
-            logger.info(' Constructed dictionaries for operator helper commands on Slack.')
 
+            self.full_name_to_id_dictionary, self.id_to_username_dictionary, self.username_to_id_dictionary = self.construct_user_dictionaries()
+            if not self.full_name_to_id_dictionary:
+                logger.critical(" Cannot get the list of users.")
+                os.exit(1)
+
+            self.channel_name_to_id_dictionary, self.channel_id_to_name_dictionary = self.construct_channel_dictionaries()
+            if not self.channel_name_to_id_dictionary:
+                logger.error(' Cannot get the list of channels, therefore cannot find the monitor channel. Related functionalities will be disabled.')
+            elif self.monitor_channel_name not in self.channel_name_to_id_dictionary:
+                logger.error(' The name of monitor channel is not found. Related functionalities will be disabled.')
+            else:
+                self.monitor_channel_id = self.channel_name_to_id_dictionary[self.monitor_channel_name]
+                logger.info(' The id for monitor channel [' + self.monitor_channel_name + '] is : ' + str(self.monitor_channel_id))
+
+            self.construct_command_dictionary()
+            self.send_message(self.monitor_channel_id, "Have no fear, @operator is here!")
+            
             creds = self.get_credentials()
             events = self.get_event_list(creds)
             if not events:
                 logger.critical(' Unable to get Google Calendar event list.')
                 os.exit(1)
             logger.info(' Successfully get ' + str(len(events)) + ' events from Google Calendar.')
-            events_update_time = datetime.datetime.now() + datetime.timedelta(minutes=30) 
+            update_time = datetime.datetime.now() + datetime.timedelta(minutes=30) 
             
             while True:
                 channel = self.parse_output(self.slack_client.rtm_read(), bot_id)
-                if datetime.datetime.now() > events_update_time:
+                if datetime.datetime.now() > update_time:
+
+                    # to-do: also update dictionaries and operator information
+
                     new_events = self.get_event_list(creds)
                     if not new_events:
                         logger.warning(' The update of Google Calendar event list fails. Information might be outdated.')
                     else:
                         events = new_events
                         logger.info(' Successfully updated the Google Calendar event list. Next update will occur 30 minutes later.')
-                    events_update_time = datetime.datetime.now() + datetime.timedelta(minutes=30)
+                    update_time = datetime.datetime.now() + datetime.timedelta(minutes=30)
                 if channel:
-                    logger.info(' @operator is called! Looking for the operator...')
+                    logger.info(' @operator is called in the channel [' + str(channel) + ']! Looking for the operator...')
                     new_operator_name = self.get_operator_name(events)
                     if not new_operator_name:
                         self.current_operator_name = ""
@@ -233,10 +283,13 @@ class AtOperator():
                         logger.info(' Found a new operator!')
                     self.at_operator(channel)
                             
+
                 time.sleep(1)
         else:
             logger.critical(" An error occurs when connecting to Slack.")
             os.exit(1)
+
+
 
 if __name__ == '__main__':
     logging.basicConfig()
