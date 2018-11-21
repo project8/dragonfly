1de2a554399bef3fee4f639930a16797dce992e0
yadiw <32943329+yadiw@users.noreply.github.com>
2018-11-20 21:58:20 -0800

    now only 1 warning per cycle; more to be fixed tomorrow

--- dragonfly/implementations/dungbeetle.py
+++ dragonfly/implementations/dungbeetle.py
@@ -28,33 +28,26 @@ class DungBeetle(Endpoint,Scheduler):
         self.root_dirs = root_dirs
         self.max_age = datetime.timedelta(**max_age)
         self.ignore_dirs = ignore_dirs
-        self.processed_dirs = {}
 
     # recursively delete empty directories
-    def del_dir(self, path, min_creation_time, processed_dirs_per_cycle):
+    def del_dir(self, path, min_creation_time, dirs_to_be_warned):
         if os.path.isdir(path):
             creation_time = datetime.datetime.fromtimestamp(os.path.getctime(path))
             no_sub_dir = True
             for item in os.listdir(path):
                 sub_path = os.path.join(path, item)
                 no_sub_dir = no_sub_dir and (not os.path.isdir(sub_path))
-                self.del_dir(sub_path, min_creation_time, processed_dirs_per_cycle)
+                self.del_dir(sub_path, min_creation_time, dirs_to_be_warned)
             if creation_time < min_creation_time and (not path in self.ignore_dirs):
                 try:
                     os.rmdir(path)
                     logger.info(" path [{}] has been removed.".format(path))
                 except OSError, err:
                     if no_sub_dir:
-                        processed_dirs_per_cycle.append(path)
-                        if path not in self.processed_dirs:
-                            self.processed_dirs[path] = 1
-                        else:
-                            self.processed_dirs[path] += 1
-                        if self.processed_dirs[path] <= 5 or self.processed_dirs[path] % 5 == 0:
-                            logger.warning(" path [{}] not removed because not empty".format(path))
+                        dirs_to_be_warned.append(path)
 
     # clean up empty directories under a specific directory without deleting itself
-    def clean_dir(self, processed_dirs_per_cycle):
+    def clean_dir(self, dirs_to_be_warned):
         logger.debug("going to clean directories")
         min_creation_time = datetime.datetime.now() - self.max_age
         for root_dir in self.root_dirs:
@@ -62,15 +55,13 @@ class DungBeetle(Endpoint,Scheduler):
             if os.path.isdir(root_dir):
                 for item in os.listdir(root_dir):
                     sub_path = os.path.join(root_dir, item)
-                    self.del_dir(sub_path, min_creation_time, processed_dirs_per_cycle)
+                    self.del_dir(sub_path, min_creation_time, dirs_to_be_warned)
             else:
                 raise Exception(" path [{}] does not exist.".format(root_dir))
 
     def scheduled_action(self):
         logger.info("doing scheduled check")
-        processed_dirs_per_cycle = []
-        self.clean_dir(processed_dirs_per_cycle)
-        for directory in list(self.processed_dirs):
-            if directory not in processed_dirs_per_cycle:
-                del self.processed_dirs[directory]
-
+        dirs_to_be_warned = []
+        self.clean_dir(dirs_to_be_warned)
+        message = str(len(dirs_to_be_warned)) + 'path(s) not removed because not empty: ' + str(dirs_to_be_warned)
+        
