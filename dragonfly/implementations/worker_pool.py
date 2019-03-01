0273c08ae0bd365953962dd85430d7928e3483c3
yadiw <32943329+yadiw@users.noreply.github.com>
2019-02-28 21:47:51 -0800

    replace the parmap with my own not-so-cool version
    does not worker inside docker :(
    TODO: maybe have some getters for multiprocessing.Manager().dict/list()

--- dragonfly/implementations/worker_pool.py
+++ dragonfly/implementations/worker_pool.py
@@ -1,52 +1,101 @@
-from _future_ import absolute_import
+from __future__ import absolute_import
 
-import parmap
+import logging
+import multiprocessing
+import time, os
+
+import itertools
 
 __all__ = []
 __all__.append('WorkerPool')
 
 logger = logging.getLogger(__name__)
 
+import copy_reg, types
+
 class WorkerPool(object):
     def __init__(self, max_workers):
-        '''
-        A maybe-too-simple class that can start a worker pool. Note that the workers should not expect themselves to interact with the endpoint anymore except getting values returned from workers. If you do want workers to share any data structure, such as append something to the same list, please use multiprocessing.Maneger() to initialize it before passing it to the worker pool.
-        max_workers: the maximum number of processes to run at the same time.
-        '''
         self.max_workers = max_workers
+        self.pool = multiprocessing.Pool(processes=self.max_workers)
 
-    def start_worker_pool(self, worker_func, worker_iterables, woker_args=(), worker_kwargs={}, parallel=True, async=False):
-        '''
-        Start a worker pool with given function, iterable(s) and constants, and close the pool after finishing it.
-        worker_func     : the reference to the function that will be run.
-        worker_iterables: a list containing iterable(s) that contains different values for different workers. For example, if you want to pass 1 to 10 to 10 workers respectively, do [[1, 2, 3, ...,10]]; if you want to pass both 1 to 10 and 101 to 110 as first to arguments to 10 workers respectively, do [[1, 2, 3, ..., 10], [101, 102, 103, ..., 110]].
-        worker_args     : positional arguments to unpack as constants that you want to pass to every worker.
-        worker_kwargs   : kwargs to unpack as constants that you want to pass to every worker.
-        parallel        : whether or not to start workers in parallel. If set to False, workers will be started sequentially.
-        async           : whether or not to execute workers asynchronously. If set to True, there's no guarantee of the order of workers.
-        '''
-        if len(iterables) > 1:
-            parmap_kwargs = {'function': worker_func,
-                             'iterables': zip(*worker_iterables),
-                             'args': worker_args,
-                             'kwargs': worker_kwargs,
-                             'pm_processes': self.max_workers,
-                             'pm_parallel': parrallel,
-                            }
-            if async:
-                return parmap.starmap_async(kwargs=parmap_kwargs)
+        def _pickle_method(m):
+            if m.im_self is None:
+                return getattr, (m.im_class, m.im_func.func_name)
             else:
-                return parmap.starmap(kwargs=parmap_kwargs)
-        else: # len(iterables) == 1
-            parmap_kwargs = {'function': worker_func,
-                             'iterable': *worker_iterables,
-                             'args': worker_args,
-                             'kwargs': worker_kwargs,
-                             'pm_processes': self.max_workers,
-                             'pm_parallel': parallel,
-                            }
-            if async:
-                return paramap.map_async(kwargs=parmap_kwargs)
-            else:
-                return paramap.map(kwargs=parmap_kwargs)
-            
+                return getattr, (m.im_self, m.im_func.func_name)
+        copy_reg.pickle(types.MethodType, _pickle_method)
+
+    #def __getstate__(self): # unnecessary in container - but let's keep it
+    #    self_dict = self.__dict__.copy()
+    #    del self_dict['pool']
+    #    return self_dict
+
+    def worker(self, pair):
+        func, args = pair
+        return func(*args)
+
+    def unpack_pack(self, func, iterables, constants):
+        iterable_len = len(iterables[0])
+        for iterable in iterables:
+            this_len = len(iterable)
+            if this_len != iterable_len:
+                logger.warning(' All iterables are supposed to have same length... I will follow the shortest one.')
+                break
+        result_list = [list(elem) for elem in (zip(*iterables))]
+        result_tuple = []
+        for i in result_list:
+            i.extend(list(constants))
+            result_tuple.append((func, i))
+        return result_tuple
+
+    def start_worker_pool(self, func, iterables=[], constants=[]):
+        logger.debug(' Trying to start a worker pool with ' +  str(iterables) + '    '+ str(constants))
+        if not (isinstance(iterables, list) and isinstance(constants, list)):
+
+            logger.error(" Don't forget to put iterables and constants in lists")
+            return
+        if len(iterables) == 0:
+            logger.warning(" I receive no iterables... Will do this sequentially.")
+            return func(*constants)
+        if len(constants) == 0:
+            logger.debug(' I get only iterables!')
+            iterable = zip(*iterables)
+            r = self.pool.map(self.worker, iterable)
+        else:
+            logger.debug(' I get both iterables and constants!')
+            args_tuple = self.unpack_pack(func, iterables, constants)
+        r =  self.pool.map(self.worker, args_tuple)
+        return r
+        
+class Class(WorkerPool): # for testing
+    def __init__(self):
+        WorkerPool.__init__(self, 5)
+
+    def something(self, num, num2, sentence, l1, a_list):
+        for i in range(num):
+            time.sleep(1)
+            print(i)
+        print('--------done-----------' + str(num))
+        print(sentence + str(num2))
+        print(l1)
+        a_list.append(num + num)
+        return num * num
+
+    def a_splendid_method(self, sth):
+        print(str(sth) + ' this is a great method!!!')
+
+    def somethingelse(self):
+        start_time = time.time()
+        a_list = multiprocessing.Manager().list()
+        r = self.start_worker_pool(self.something, [range(10), range(15)], ['I am cool!', 'something', a_list])
+        print("--- %s seconds ---" % (time.time() - start_time))
+        print(a_list)
+        return r
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    logger.setLevel(logging.DEBUG)
+    c = Class()
+    result = c.somethingelse()
+   
+    
