4c172154170ca537977cdf744169789fbe029447
yadiw <15810229308@163.com>
2019-03-06 13:44:01 -0800

    add comments to worker pool, and close the pool everytime.

--- dragonfly/implementations/worker_pool.py
+++ dragonfly/implementations/worker_pool.py
@@ -15,8 +15,18 @@ import copy_reg, types
 
 class WorkerPool(object):
     def __init__(self, max_workers):
+        '''
+        A maybe-too-simple class that can start a worker pool. 
+        Note that the workers should not expect themselves to interact with the 
+        endpoint anymore, except getting values returned from workers. 
+        If you do want workers to share any data structure, such as appending 
+        something to the same list, please use multiprocessing.Maneger() to 
+        initialize it before passing it to the worker pool.
+
+        max_workers: the maximum number of processes to run at the same time.
+        '''
         self.max_workers = max_workers
-        self.pool = multiprocessing.Pool(processes=self.max_workers)
+
 
         def _pickle_method(m):
             if m.im_self is None:
@@ -25,16 +35,26 @@ class WorkerPool(object):
                 return getattr, (m.im_self, m.im_func.func_name)
         copy_reg.pickle(types.MethodType, _pickle_method)
 
-    #def __getstate__(self): # unnecessary in container - but let's keep it
+    #def __getstate__(self): # unnecessary in container - but let's keep it for now
     #    self_dict = self.__dict__.copy()
     #    del self_dict['pool']
     #    return self_dict
 
     def worker(self, pair):
+        '''
+        Wrap a given function requiring multiple parameters into a function 
+        requiring only one parameters, and return it.
+
+        pair: a tuple containing a worker function and a list of its parameters.
+        '''
         func, args = pair
         return func(*args)
 
     def unpack_pack(self, func, iterables, constants):
+        '''
+        Unpack given lists of iterables and constants, and pack them again along with
+        the given function. Return a list of  tuples each containning a worker function and a list of its parameters.  
+        ''' 
         iterable_len = len(iterables[0])
         for iterable in iterables:
             this_len = len(iterable)
@@ -49,21 +69,33 @@ class WorkerPool(object):
         return result_tuple
 
     def start_worker_pool(self, func, iterables=[], constants=[]):
+        '''
+        Start a worker pool with given function, iterable(s) and constants, 
+        and close it after every worker finishes its job.
+        Return a list containing values being returned by each worker.
+
+        func     : the reference to the function that will be run.
+        iterables: a list containing iterable(s) that contains different values for different workers. For example, if you want to pass 1 to 10 to 10 workers respectively, do [[1, 2, 3, ...,10]]; if you want to pass both 1 to 10 and 101 to 110 as first to arguments to 10 workers respectively, do [[1, 2, 3, ..., 10], [101, 102, 103, ..., 110]].
+        constants: a list containing constants that you want to pass to every worker.
+        '''
         logger.debug(' Trying to start a worker pool with ' +  str(iterables) + '    '+ str(constants))
+        pool = multiprocessing.Pool(processes=self.max_workers)
         r = None
         if not (isinstance(iterables, list) and isinstance(constants, list)):
-            logger.error(" Don't forget to put iterables and constants in lists")
+            logger.error(" Don't forget to put iterables and constants in lists!")
         elif len(iterables) == 0:
             logger.warning(" I receive no iterables... Will do this sequentially.")
             r = func(*constants)
         elif len(constants) == 0:
             logger.debug(' I get only iterables!')
             iterable = zip(*iterables)
-            r = self.pool.map(self.worker, iterable)
+            r = pool.map(self.worker, iterable)
         else:
             logger.debug(' I get both iterables and constants!')
             args_tuple = self.unpack_pack(func, iterables, constants)
-            r =  self.pool.map(self.worker, args_tuple)
+            r =  pool.map(self.worker, args_tuple)
+        pool.close()
+        pool.join()
         return r
         
 class Class(WorkerPool): # for testing
