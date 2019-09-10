2fcbb9c6f519b518afdd2aedafe44ed7263f48dc
Ben LaRoque <benjamin.laroque@gmail.com>
2019-09-10 15:48:31 -0700

    allow specifying the binding key

--- dragonfly/implementations/sensor_logger.py
+++ dragonfly/implementations/sensor_logger.py
@@ -21,17 +21,18 @@ class SensorLogger(Gogol, PostgreSQLInterface):
     '''
     Project 8 slow control table implementation
     '''
-    def __init__(self, sensor_type_map_table, sensor_type_column_name='type', sensor_type_match_column='endpoint_name', data_tables_dict={}, **kwargs):
+    def __init__(self, sensor_type_map_table, sensors_key='sensor_value', sensor_type_column_name='type', sensor_type_match_column='endpoint_name', data_tables_dict={}, **kwargs):
         '''
         sensor_type_map_table (str): name of the child endpoint of this instance which provides access to the endpoint_id_map, which stores the sensor type
         sensor_type_column_name (str): name of the column to use for the return type (matched against keys in the data_tables_dict argument here)
         sensor_type_match_column (str): column against which to check for matches to the sensor name
         data_tables_dict (dict): dictionary mapping types (in the sensor_type_map_table) to child endpoints of this instance which provide access to the data_table for that type
         '''
-        # listen to sensor_value alerts channel
-        kwargs.update({'keys':['sensor_value.#']})
+        self.prefix = sensors_key
+        kwargs.update({'keys':['{}.#'.format(sensors_key)]})
         PostgreSQLInterface.__init__(self, **kwargs)
         Gogol.__init__(self, **kwargs)
+        logger.debug('bindings are: {}'.format(self._bindings))
 
         self._sensor_type_map_table = sensor_type_map_table
         self._sensor_type_column_name = sensor_type_column_name
@@ -49,10 +50,14 @@ class SensorLogger(Gogol, PostgreSQLInterface):
 
 
     def this_consume(self, message, basic_deliver):
+        logger.debug("consuming message to: {}".format(basic_deliver.routing_key))
         ### Get the sensor name
+        if not basic_deliver.routing_key.split('.')[0] == self.prefix:
+            logger.warning("should not consume this message")
+            return
         sensor_name = None
         if '.' in basic_deliver.routing_key:
-            re_out = re.match(r'sensor_value.(?P<from>\S+)', basic_deliver.routing_key)
+            re_out = re.match(r'{}.(?P<from>\S+)'.format(self.prefix), basic_deliver.routing_key)
             sensor_name = re_out.groupdict()['from']
         # note that the following is deprecated in dripline 2.x, retained for compatibility
         else:
@@ -72,6 +77,9 @@ class SensorLogger(Gogol, PostgreSQLInterface):
                                              where_eq_dict={self._sensor_type_match_column:sensor_name},
                                             )
             self._sensor_types[sensor_name] = this_type[1][0][0]
+            if not self._sensor_types[sensor_name] in self._data_tables:
+                logger.critical('endpoint with name "{}" is not configured with a recognized type in the sensors_list table'.format(sensor_name))
+                return
             this_data_table = self.endpoints[self._data_tables[self._sensor_types[sensor_name]]]
 
             ### Log the sensor value
