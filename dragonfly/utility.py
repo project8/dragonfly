bc5ea6c39ab01ced01c805dcc7f02e7c7f8823bb
Rene Reimann <rreimann@uni-mainz.de>
2025-07-16 11:30:16 +0200

    add that we can overwrite an elog entry, we will use that for testing to not swarm the elog

--- dragonfly/utility.py
+++ dragonfly/utility.py
@@ -2,7 +2,10 @@ import elog
 from getpass import getpass
 
 
-def send_to_elog(message, subject="Checklist", author="Rene Reimann", category="Slow Controls"):
+def send_to_elog(message, subject="Checklist", author="Rene Reimann", category="Slow Controls", msg_id=None):
     password = getpass()
     logbook = elog.open("https://maxwell.npl.washington.edu/elog/project8/P8+Mainz+lab", user="rreimann", password=password)
-    return logbook.post(message, subject=subject, Author=author, category=category, suppress_email_notification=True)
+    if msg_id is None:
+       return logbook.post(message, subject=subject, Author=author, category=category, suppress_email_notification=True)
+    else:
+       return logbook.post(message, msg_id=msg_id, subject=subject, Author=author, category=category, suppress_email_notification=True)
