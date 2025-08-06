0cca0681cc27e29185bdff08c7b4a03ccc1821ac
Rene Reimann <rreimann@uni-mainz.de>
2025-08-06 13:25:25 +0200

    adding start run and stop run, adding logging in database, fixing issues

--- dripline/extensions/run_state.py
+++ dripline/extensions/run_state.py
@@ -1,9 +1,9 @@
 import threading
 import os
 import json
-from dripline.core import Service, ThrowReply, Entity
-
+from dripline.core import Service, ThrowReply, Entity, MsgAlert
 import logging
+import scarab
 logger = logging.getLogger(__name__)
 
 __all__ = []
@@ -15,7 +15,7 @@ class StateGetEntity(Entity):
         Entity.__init__(self, **kwargs)
     
     def on_get(self):
-        return self.service.get_state()[key]
+        return self.service.get_state()[self.key]
 
     def on_set(self, value):
         raise ThrowReply("on_set_error", f"on_set not available for {self.name}")
@@ -40,13 +40,13 @@ class RunStateService(Service):
             raise ThrowReply('service_error_invalid_value', f"Invalid state file: <{state_file}>! Expect string")
 
         self.alock = threading.Lock()
-        self.state_file
+        self.state_file = state_file
         
     def get_state(self):
         '''
         Read the state of the run from a file.
         '''
-        logger.info(f"Ethernet socket {self.socket_info} established")
+        logger.info(f"reading state")
         self.alock.acquire()
         try:
             if os.path.exists(self.state_file):
@@ -62,7 +62,36 @@ class RunStateService(Service):
             self.alock.release()
         return state
 
-    def save_state(self):
+    def save_state(self, state):
         with open(self.state_file, "w") as open_file:
             json.dump(state, open_file)
         return
+
+    def stop_run(self):
+        state = self.get_state()
+        if not state["run_active"]:
+            logger.info("There is no run to stop")
+        else:
+            state["run_active"] = False
+            self.save_state(state)
+            logger.info(f'Stopped run {state["run_number"]}')
+        return state["run_number"]
+
+    def start_run(self, comment):
+        logger.info(f"Starting run with comment {comment}")
+        state = self.get_state()
+        if state["run_active"]:
+            self.stop_run()
+            
+        state["run_number"] = state["run_number"]+1
+        state["run_comment"] = comment
+        state["run_active"] = True
+        self.save_state(state)
+        the_alert = MsgAlert.create(payload=scarab.to_param(state["run_number"]), routing_key=f'sensor_value.run_number')
+        alert_sent = self.send(the_alert)
+        the_alert = MsgAlert.create(payload=scarab.to_param(state["run_comment"]), routing_key=f'sensor_value.run_comment')
+        alert_sent = self.send(the_alert)
+        the_alert = MsgAlert.create(payload=scarab.to_param(state["run_active"]), routing_key=f'sensor_value.run_active')
+        alert_sent = self.send(the_alert)
+        logger.info(f'Started run {state["run_number"]} with comment: {state["run_comment"]}')
+        return state["run_number"]
