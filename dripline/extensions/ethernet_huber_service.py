cccff876d9730ffea42fdbe4a5b5485a99bbfb9b
Rene Reimann <rreimann@uni-mainz.de>
2025-09-02 15:37:02 +0200

    adding Huber service and entity

---
+++ dripline/extensions/ethernet_huber_service.py
@@ -0,0 +1,134 @@
+import time
+
+from dripline.core import ThrowReply, Entity, calibrate
+from dripline.implementations import EthernetSCPIService
+
+import logging
+logger = logging.getLogger(__name__)
+
+__all__ = []
+
+__all__.append('EthernetHuberService')
+
+def int_to_hexstr(value):
+    return hex(value)[2:].zfill(2)
+
+def hexstr_to_bytes(value):
+    return bytes.fromhex(value)
+
+def bytes_to_hexstr(value):
+    return value.hex()
+
+def hexstr_to_int(value):
+    return int(value, 16)
+
+def bytes_to_ints(value):
+    return [byte for byte in value]
+
+class EthernetHuberService(EthernetSCPIService):
+    '''
+    A fairly specific subclass of Service for connecting to ethernet-capable thermo fisher devices.
+    In particular, devices must support a half-duplex serial communication with header information, variable length data-payload and a checksum.
+    '''
+    def __init__(self, **kwargs):
+        '''
+        Args:
+            socket_timeout (int): number of seconds to wait for a reply from the device before timeout.
+            socket_info (tuple or string): either socket.socket.connect argument tuple, or string that
+                parses into one.
+        '''
+        EthernetSCPIService.__init__(self, **kwargs)
+
+    def calculate_checksum(self, input_string):
+        """
+        Calculates the 1-byte checksum of the input string.
+        Returns the checksum as a 2-character uppercase hex string.
+
+        :param input_string: The string to compute the checksum for
+        :return: Checksum as a hex string (e.g., 'C6')
+        """
+        total = sum(ord(char) for char in input_string)
+        checksum = total % 256
+        return f"{checksum:02X}"
+    
+    def check_checksum(self, cmd):
+        # calculate checksum of response except checksum and check if match checksum
+        return self.calculate_checksum( cmd[:-2] ) == cmd[-2:]
+
+
+    def _assemble_cmd(self, cmd_in):
+        cmd_raw = cmd_in.split(" ")[0]
+        data = " ".join(cmd_in.split(" ")[1:])
+        cmd = "[M01" + cmd_raw
+        length = len(cmd) + len(data) + 2
+        cmd = cmd + f"{length:02X}" + data
+        cs = self.calculate_checksum(cmd)
+        cmd = cmd + cs + self.command_terminator
+        return cmd
+
+    def _extract_reply(self, data):
+        return data
+
+
+    def _send_commands(self, commands):
+        '''
+        Take a list of commands, send to instrument and receive responses, do any necessary formatting.
+
+        commands (list||None): list of command(s) to send to the instrument following (re)connection to the instrument, still must return a reply!
+                             : if impossible, set as None to skip
+        '''
+        all_data=[]
+
+        for command in commands:
+            command = self._assemble_cmd(command)
+            logger.debug(f"sending: {command.encode()}")
+            self.socket.send(command.encode())
+            if command == self.command_terminator:
+                blanck_command = True
+            else:
+                blanck_command = False
+
+            data = self._listen(blanck_command)
+            
+            if self.reply_echo_cmd:
+                if data.startswith(command):
+                    data = data[len(command):]
+                elif not blank_command:
+                    raise ThrowReply('device_error_connection', f'Bad ethernet query return: {data}')
+            logger.info(f"sync: {repr(command)} -> {repr(data)}")
+            data = self._extract_reply(data)
+            all_data.append(data)
+        return all_data
+
+
+__all__.append("HuberEntity")
+class HuberEntity(Entity):
+    '''
+    A endpoint of a Huber device that returns the request result
+    '''
+
+    def __init__(self,
+                 get_str=None,
+                 **kwargs):
+        '''
+        Args:
+            get_str: hexstring of the command, e.g. 20
+        '''
+        if get_str is None:
+            raise ValueError('<get_str is required to __init__ ThermoFisherHexGetEntity instance')
+        else:
+            self.cmd_str = str(get_str).zfill(2)
+        Entity.__init__(self, **kwargs)
+
+    @calibrate()
+    def on_get(self):
+        # setup cmd here
+        to_send = [self.cmd_str]
+        logger.debug(f'Send cmd in hexstr: {to_send[0]}')
+        result = self.service.send_to_device(to_send)
+        logger.debug(f'raw result is: {result}')
+
+        return result
+
+    def on_set(self, value):
+        raise ThrowReply('message_error_invalid_method', f"endpoint '{self.name}' does not support set")
