248d66d38407c71f605e22bd56ced8abbe35e6a7
Rene Reimann <rreimann@uni-mainz.de>
2026-02-03 19:36:56 +0100

    make password an optional parameter

--- dragonfly/utility.py
+++ dragonfly/utility.py
@@ -2,8 +2,9 @@ import elog
 from getpass import getpass
 
 
-def send_to_elog(message, subject="Checklist", author="Rene Reimann", category="Slow Controls", msg_id=None):
-    password = getpass()
+def send_to_elog(message, subject="Checklist", author="Rene Reimann", category="Slow Controls", msg_id=None, password=None):
+    if password is None:
+        password = getpass()
     logbook = elog.open("https://maxwell.npl.washington.edu/elog/project8/P8+Mainz+lab", user="rreimann", password=password)
     if msg_id is None:
        return logbook.post(message, subject=subject, Author=author, category=category, suppress_email_notification=True)
