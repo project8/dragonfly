2315f1a70c6b671ddc736b51a90727207b0cd86e
Rene Reimann <rreimann@uni-mainz.de>
2026-02-04 22:29:30 +0100

    this implementation now looks up the table type in the id_map table.

--- dripline/extensions/project8_sensor_logger.py
+++ dripline/extensions/project8_sensor_logger.py
@@ -6,6 +6,7 @@ from __future__ import absolute_import
 
 # standard libs
 import logging
+import re
 
 # 3rd party libs
 import sqlalchemy
@@ -22,17 +23,17 @@ __all__.append('Project8SensorLogger')
 class Project8SensorLogger(AlertConsumer, PostgreSQLInterface):
     '''
     A custom sensor logger tailored to the project 8 database structure.
-    An alert consumer which converts alert messages into database insertions.
-
-    The assumption made is that complex logic dealing with organization or structure of the particular database should live in the database itself (possibly in a view, with a trigger, ...) and that here we can simply do an insert.
     '''
-    def __init__(self, insertion_table_endpoint_name, **kwargs):
+    def __init__(self, sensor_type_map_table, data_tables_dict={}, **kwargs):
         '''
+        sensor_type_map_table (str): name of the child endpoint of this instance which provides access to the endpoint_id_map, which stores the sensor type
+        data_tables_dict (dict): dictionary mapping types (in the sensor_type_map_table) to child endpoints of this instance which provide access to the data_table for that type
         '''
         AlertConsumer.__init__(self, add_endpoints_now=False, **kwargs)
         PostgreSQLInterface.__init__(self, **kwargs)
-
-        self.insertion_table_endpoint_name = insertion_table_endpoint_name
+        
+        self._sensor_type_map_table = sensor_type_map_table
+        self._data_tables = data_tables_dict
 
         self.connect_to_db(self.auth)
 
@@ -45,12 +46,21 @@ class Project8SensorLogger(AlertConsumer, PostgreSQLInterface):
 
     def process_payload(self, a_payload, a_routing_key_data, a_message_timestamp):
         try:
-            this_data_table = self.sync_children[self.insertion_table_endpoint_name]
+            # get the type and table for the sensor
+            this_type = None
+            this_type = self.sync_children[self._sensor_type_map_table].do_select(return_cols=["type"], 
+                                                                                  where_eq_dict=a_routing_key_data)
+            #if len(this_type) != 1:
+            #    raise Exception("not registered endpoint")
+            table_name = self._data_tables[this_type[1][0][0]]
+            this_data_table = self.sync_children[table_name]
+
             # combine data sources
             insert_data = {'timestamp': a_message_timestamp}
             insert_data.update(a_routing_key_data)
             insert_data.update(a_payload.to_python())
-            logger.info(f"Inserting from endpoint {self.insertion_table_endpoint_name}; data are:\n{insert_data}")
+            logger.info(f"Inserting {a_routing_key_data} in table {table_type}; data are:\n{insert_data}")
+
             # do the insert
             #insert_return = this_data_table.do_insert(**insert_data)
             #logger.debug(f"Return from insertion: {insert_return}")
