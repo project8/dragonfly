de59eacf61b19b76ae4622f8486a66003c2a6af1
Rene Reimann <rreimann@uni-mainz.de>
2026-02-04 15:42:29 +0100

    adding a custom project8 sensor logger, currently we just start with a copy of the normal PostgresSensorLogger with commented out insert. We will taylor it to the project8 database

---
+++ dripline/extensions/project8_sensor_logger.py
@@ -0,0 +1,61 @@
+'''
+A Postgres Interface-based logger
+'''
+
+from __future__ import absolute_import
+
+# standard libs
+import logging
+
+# 3rd party libs
+import sqlalchemy
+
+# internal imports
+from dripline.core import AlertConsumer
+from dripline.implementations.postgres_interface import PostgreSQLInterface
+
+__all__ = []
+logger = logging.getLogger(__name__)
+
+
+__all__.append('Project8SensorLogger')
+class Project8SensorLogger(AlertConsumer, PostgreSQLInterface):
+    '''
+    A custom sensor logger tailored to the project 8 database structure.
+    An alert consumer which converts alert messages into database insertions.
+
+    The assumption made is that complex logic dealing with organization or structure of the particular database should live in the database itself (possibly in a view, with a trigger, ...) and that here we can simply do an insert.
+    '''
+    def __init__(self, insertion_table_endpoint_name, **kwargs):
+        '''
+        '''
+        AlertConsumer.__init__(self, add_endpoints_now=False, **kwargs)
+        PostgreSQLInterface.__init__(self, **kwargs)
+
+        self.insertion_table_endpoint_name = insertion_table_endpoint_name
+
+        self.connect_to_db(self.auth)
+
+        self.add_endpoints_from_config()
+
+    # add_endpoint is a mess here because of the diamond inheritance, so let's be explicit
+    def add_child(self, endpoint):
+        AlertConsumer.add_child(self, endpoint)
+        self.add_child_table(endpoint)
+
+    def process_payload(self, a_payload, a_routing_key_data, a_message_timestamp):
+        try:
+            this_data_table = self.sync_children[self.insertion_table_endpoint_name]
+            # combine data sources
+            insert_data = {'timestamp': a_message_timestamp}
+            insert_data.update(a_routing_key_data)
+            insert_data.update(a_payload.to_python())
+            logger.info(f"Inserting from endpoint {self.insertion_table_endpoint_name}; data are:\n{insert_data}")
+            # do the insert
+            #insert_return = this_data_table.do_insert(**insert_data)
+            #logger.debug(f"Return from insertion: {insert_return}")
+            logger.info("finished processing data")
+        except sqlalchemy.exc.SQLAlchemyError as err:
+            logger.critical(f'Received SQL error while doing insert: {err}')
+        except Exception as err:
+            logger.critical(f'An exception was raised while processing a payload to insert: {err}')
