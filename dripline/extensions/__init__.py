db97c6d2be110d881b5da0041c82b5d14113689d
Paul Kolbeck <kolbeckpaul@gmail.com>
2026-02-13 10:40:54 -0800

    Adjusted init

--- dripline/extensions/__init__.py
+++ dripline/extensions/__init__.py
@@ -1,1 +1,13 @@
+__all__ = []
+
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+# Subdirectories
+
+# Modules in this directory
+
+from .daq_run_interface import *
+from .psyllid_provider import *
+from .r2daq import *
+from .roach_daq_run_interface import *
+from .roach2_interface import *
