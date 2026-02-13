47725eb3f2a536e394572f45be8bdfd441dbb2ac
Paul Kolbeck <kolbeckpaul@gmail.com>
2026-02-13 11:44:02 -0800

    Adding try except block to catch missing dependencies

--- dripline/extensions/__init__.py
+++ dripline/extensions/__init__.py
@@ -5,12 +5,17 @@ __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 # Subdirectories
 
 # Modules in this directory
-
-from .daq_run_interface import *
-from .psyllid_provider import *
-from .r2daq import *
-from .roach_daq_run_interface import *
-from .roach2_interface import *
+try:
+    import katcp_wrapper
+    import r2daq
+except ImportError:
+    print("Missing DAQ dependency(s). Not importing DAQ-related modules.")
+else:
+    from .daq_run_interface import *
+    from .psyllid_provider import *
+    from .r2daq import *
+    from .roach_daq_run_interface import *
+    from .roach2_interface import *
 from .add_auth_spec import *
 from .cmd_endpoint import *
 from .asteval_endpoint import *
