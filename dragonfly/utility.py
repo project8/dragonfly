cdb521d3afecd43f71e0a536de707bdec5b827b2
Rene Reimann <rreimann@uni-mainz.de>
2026-02-26 16:29:48 +0100

    fixing user authentication, had hardcoded name before, now use from config

--- dragonfly/utility.py
+++ dragonfly/utility.py
@@ -5,7 +5,7 @@ from getpass import getpass
 def send_to_elog(message, subject="Checklist", author="Rene Reimann", category="Slow Controls", msg_id=None, password=None):
     if password is None:
         password = getpass()
-    logbook = elog.open("https://maxwell.npl.washington.edu/elog/project8/P8+Mainz+lab", user="rreimann", password=password)
+    logbook = elog.open("https://maxwell.npl.washington.edu/elog/project8/P8+Mainz+lab", user=author, password=password)
     if msg_id is None:
        return logbook.post(message, subject=subject, Author=author, category=category, suppress_email_notification=True)
     else:
