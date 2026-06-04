2b3744f684c389f89a70ebf70a5fc3174915cd2e
Noah Oblath <noah.oblath@pnnl.gov>
2026-06-04 10:22:16 -0700

    Bumped version to 2.2.0

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ print('packages are: {}'.format(packages))
 
 setup(
     name="dragonfly",
-    version='v2.1.1',  # TODO: should get version from git
+    version='v2.2.0',  # TODO: should get version from git
     packages=packages,
 )
